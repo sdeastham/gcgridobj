75320d89b63078fda639b0083c19c02b1ff9265f
Sebastian Eastham <seastham@fas.harvard.edu>
2020-10-22 17:39:28 -0400

    Fixed typo in setup.py (pointed to wrong README)
    setup.py was pointing to README.txt rather than README.md, causing
    setup to fail. This has been fixed.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A package which provides grid objects for working with GEOS-Chem data',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     install_requires=['numpy'],
     url='https://github.com/sdeastham/gcgridobj',
     author='Sebastian David Eastham',
